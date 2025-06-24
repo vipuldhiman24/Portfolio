f60f58e450b819089b987b86e77a71a3331fa8c9
vipul dhiman <vipuldhiman24@gmail.com>
2025-06-24 20:31:27 +0530

    made changes to drak mode

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Projects.css';
+import {FaGithub } from 'react-icons/fa';
 
 const projectList = [
-  { title: 'Quiz App Backend', image: '', github: '', live: '' },
-  { title: 'Employee Management System', image: '', github: '', live: '' },
+  { title: 'Springboot and React Authorization', image: '', github: 'https://github.com/vipuldhiman24/auth-service-springboot.git', live: '' },
+  { title: 'Employee Management System', image: '', github: 'https://github.com/vipuldhiman24/employee-management-backend.git', live: '' },
   { title: 'Movie Tracker App', image: '', github: '', live: '' },
 ];
 
@@ -17,8 +18,8 @@ const Projects = () => {
             <div key={index} className="project-card">
               <h3>{title}</h3>
               <div className="project-links">
-                <a className="btn" href={github || "#"} target="_blank" rel="noreferrer">Code</a>
-                <a className="btn" href={live || "#"} target="_blank" rel="noreferrer">Live Demo</a>
+                <a className="btn" href={github || "#"} target="_blank" rel="noreferrer"><FaGithub  size={28} /></a>
+                
               </div>
             </div>
           ))}
